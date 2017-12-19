979d0e6534a374736fa7c0822c3597a855b48c2c
Liam Houghton <liamhoughton90@yahoo.com>
2017-12-19 15:10:25 -0600

    Completed clock and alarm and radio

--- WorkingClock/Clock.cs
+++ WorkingClock/Clock.cs
@@ -13,37 +13,37 @@ namespace WorkingClock
             string date = DateTime.Now.ToString("h:mm:ss tt");
             return date;
         }
-        /*public string DisplayTime()
-        {
-            string gettingTime = Time();
-            Console.WriteLine(gettingTime);
-        }*/
         string Radio()
         {
             Console.WriteLine("What radio staion do you want?");
             string answer = Console.ReadLine();
             return answer;
         }
-        /*public void DisplayRadio()
-        {
-            string gettingRadio = Radio();
-            Console.WriteLine(gettingRadio)
-        }*/
         string AlarmClock()
         {
-            Console.WriteLine("What time do you want to set an alarm to?");
+            Console.WriteLine("What time do you want to set an alarm to? DONT ADD AM OR PM");
             string alarmTime = Console.ReadLine();
             float.Parse(alarmTime);
             return alarmTime;
+        }
+        string AlarmAmPm()
+        {
+            Console.WriteLine("AM or PM?");
+            string pmam = Console.ReadLine();
+            return pmam;
         }
         public void DisplayEverything()
         {
             string gettingTime = Time();
             Console.WriteLine(gettingTime);
             string gettingRadio = Radio();
-            Console.WriteLine("This Staion is playing: ", gettingRadio);
+            Console.WriteLine("This Staion is playing: "); 
+            Console.WriteLine(gettingRadio);
             string gettingAlarmClock = AlarmClock();
-            Console.WriteLine("You the alarm to go off at this time: ", gettingAlarmClock);
+            string gettingAmPm = AlarmAmPm();
+            Console.WriteLine("You the alarm to go off at this time: ");
+            Console.WriteLine(gettingAlarmClock + gettingAmPm);
+
         }
     }
 }
