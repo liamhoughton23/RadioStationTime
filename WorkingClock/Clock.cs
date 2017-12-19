3845083957ee875f038642da005a236d8d6008e3
Liam Houghton <liamhoughton90@yahoo.com>
2017-12-19 10:58:34 -0600

    First Commit radio

---
+++ WorkingClock/Clock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingClock
+{
+    class Clock
+    {
+        public string Time()
+        {
+            string date = DateTime.Now.ToString("h:mm:ss tt");
+            return date;
+        }
+        public void DisplayTime()
+        {
+            string gettingTime = Time();
+            Console.WriteLine(gettingTime);
+            Console.ReadKey();
+
+        }
+  
+    }
+}
---
+++ WorkingClock/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock radio = new Clock();
+            radio.DisplayTime();
+            Console.ReadKey();
+
+        }
+    }
+}
